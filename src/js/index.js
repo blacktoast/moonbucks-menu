e17f0d5a1287dda7f1dfefff11c2ff83b8398bdb
blacktoast <dbstjd100@naver.com>
2021-10-18 13:20:35 +0900

    ttt

--- src/js/index.js
+++ src/js/index.js
@@ -39,7 +39,7 @@ let removeInput=(input)=>{
     $(input).value="";
 }
 function insertHtml(item,dest,func){
-dest.insertAdjacentHTML("beforeend",func(item));
+  dest.insertAdjacentHTML("beforeend",func(item));
 }
 
 function counter(){
@@ -56,12 +56,23 @@ function addMenuName(input,toList){
     let menuName=$(input).value;
     let chk=checkInput(menuName);
     if(chk){ insertHtml(menuName,toList,returnMenuItem);
+    
     let editBtn=$(".menu-edit-button"); 
     console.log(editBtn);
     removeInput(input);
-    let menuCounter=$a(".menu-list-item").length;
-    $(".menu-count").innerText=`총 ${menuCounter}개`;
+    updateCount();
 }
+}
+
+function updateMenuName(e){
+        const $menuName=e.target.closest("li").querySelector(".menu-name");
+        const updatedMenuName=prompt("메뉴수정을 해주세요",$menuName.innerText);     
+        $menuName.innerText=updatedMenuName;
+}
+
+function updateCount(){
+  let menuCounter=$a(".menu-list-item").length;
+  $(".menu-count").innerText=`총 ${menuCounter}개`;
 }
 
 
@@ -69,26 +80,35 @@ function App(){
     let menuName;
     let espMenuList=$("#espresso-menu-list");
     const epsMenuName="#espresso-menu-name";
-    $("#espresso-menu-form")
-            .addEventListener("submit",(e)=>{
-                                    e.preventDefault();
-                                })
-    $(epsMenuName)
-    .addEventListener("keypress",(e)=>{
+
+    //수정 함수
+    espMenuList.addEventListener("click",(e)=>{
+      if(e.target.classList.contains("menu-edit-button"))
+      {
+        updateMenuName(e);
+      }
+      })
+    espMenuList.addEventListener("click",(e)=>{
+      if(e.target.classList.contains("menu-remove-button")){
+        if(confirm("hi")){
+          e.target.closest("li").remove();
+          updateCount();
+
+        }
+      }
+    })
+
+    $("#espresso-menu-form").addEventListener("submit",(e)=>{e.preventDefault();})
+    $(epsMenuName).addEventListener("keypress",(e)=>{
         if(e.key!=="Enter"){
             return;
         }
         addMenuName(epsMenuName,espMenuList);
-   
-
-            
-        }
-        
-    )
-    $("#espresso-menu-submit-button").addEventListener("click",(e)=>{
-            addMenuName();
+      })
+    
+      $("#espresso-menu-submit-button").addEventListener("click",(e)=>{
+            addMenuName(epsMenuName,espMenuList);
     })
-    //bntSubmit.addEventListener("submit",)
 
 }
 App();
